c845f7f452787acaed72f3f4f5093539a692eed9
RahmadRR <rahmadramadhankutil@gmail.com>
2025-09-13 03:36:31 +0700

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,34 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.vecteezy.com',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
